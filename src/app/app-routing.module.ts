67dfb634d8a20ee8413631a3f9e780609f17a47c
Fran Prieto <franprietogutweb@gmail.com>
2022-09-07 00:18:14 +0200

    set routes system ans some styles for app-component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ByCapitalsComponent } from './pais/pages/by-capitals/by-capitals.component';
+import { ByRegionComponent } from './pais/pages/by-region/by-region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'regions', component: ByRegionComponent },
+  { path: 'capitals', component: ByCapitalsComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
