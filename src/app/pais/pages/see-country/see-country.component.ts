8b8be1bde076a9d24cb190581b47bee9421a2f4f
Fran Prieto <franprietogutweb@gmail.com>
2022-09-19 18:41:40 +0200

    getting country by code working

--- src/app/pais/pages/see-country/see-country.component.ts
+++ src/app/pais/pages/see-country/see-country.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-see-country',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeeCountryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activatedRoute:ActivatedRoute,
+              private _countryService:CountryService) { }
 
   ngOnInit(): void {
+
+    /*we could make params.id to access the value of the id parameter,
+    but we will do with detructuraion
+    it seems we must use () to surround the {id}, which is the way to detructurate an object
+    */
+    this._activatedRoute.params.subscribe(({id})=>{
+      console.log (id);
+      this._countryService.getCountryByCode(id).subscribe(value=>{
+        console.log (value);
+      });
+
+    })
   }
 
 }
--- src/app/pais/services/country.service.ts
+++ src/app/pais/services/country.service.ts
@@ -12,6 +12,7 @@ export class CountryService {
   public byCountryUrl=`${environment.apiUrl}/name`;
   public byCapitalUrl=`${environment.apiUrl}/capital`;
   public byRegionUrl=`${environment.apiUrl}/region`;
+  public countryByCodeUrl=`${environment.apiUrl}/alpha`;
 
 
   
@@ -30,6 +31,13 @@ export class CountryService {
 
   searchByRegion(region:string):Observable<Country[]>{
     return this._http.get<Country[]>(`${this.byRegionUrl}/${region}`)
+  }
+
+  //in older api by code it returns only an object, but here also an array
+  getCountryByCode(code:string):Observable<Country[]>{
+
+    return this._http.get<Country[]>(`${this.countryByCodeUrl}/${code}`)
+
   }
   
 }
