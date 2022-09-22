a252a44baca5a93f4b97148a1c4ec66cc904f2c8
Fran Prieto <franprietogutweb@gmail.com>
2022-09-23 01:34:07 +0200

    working animation on countries table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
     // import HttpClientModule after BrowserModule.
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     SharedModule,
     PaisModule
   ],
--- src/app/pais/components/countries-table/countries-table.component.ts
+++ src/app/pais/components/countries-table/countries-table.component.ts
@@ -1,21 +1,70 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Country } from '../../interfaces/i-country';
 import { CountryService } from '../../services/country.service';
+import { AfterViewInit, ChangeDetectorRef } from '@angular/core';
+
+import {
+  trigger, state, style, animate, transition, query, group
+ } from '@angular/animations';
 
 @Component({
   selector: 'app-countries-table',
   templateUrl: './countries-table.component.html',
-  styleUrls: ['./countries-table.component.css']
+  styleUrls: ['./countries-table.component.css'],
+  animations: [
+    trigger('fade', [
+      state('hide', style({
+        
+        opacity: 0
+        
+      })),
+      state('show', style({
+        
+        opacity: 1
+      })),
+      transition('hide =>show', [
+        animate('0.5s')
+      ])/*,
+        this wont be used, component is simply destroyed when we 
+        pick another option in menu, no animation will be shown
+      transition('show => hide', [
+        animate('2s')
+      ])*/
+    ]),
+   
+  ]
 })
 export class CountriesTableComponent implements OnInit {
 
   @Input() dataFromParent:Country[];
+
+  public showTable:boolean;
   
-  constructor(public _countryService:CountryService) { 
+  constructor(public _countryService:CountryService,     private changeRef: ChangeDetectorRef
+    ) { 
     this.dataFromParent=[];
+    this.showTable=false;
+
   }
 
   ngOnInit(): void {
+
+
+  /*  this.isStarted=true;
+    console.log ('init',this.isStarted);*/
+  }
+
+  //to make animation work, must be here, not on ngOnInit
+  ngAfterViewInit(): void {
+    this.showTable=true;
+    this.changeRef.detectChanges();//to fix error https://github.com/angular/angular/issues/36173
+    console.log ('init',this.showTable);
+  }
+
+  ngOnDestroy(){
+   // this.showTable=false;
+    console.log ('destroy',this.showTable);
+
   }
 
 }
