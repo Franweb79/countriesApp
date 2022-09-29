f018ed6333954a5dac038af25335c9662cc060cf
Fran Prieto <franprietogutweb@gmail.com>
2022-09-30 01:39:37 +0200

    country service cleaned

--- src/app/pais/services/country.service.ts
+++ src/app/pais/services/country.service.ts
@@ -16,12 +16,13 @@ export class CountryService {
 
 
   /*
-    we will need them to retrieve only certain data on some methods
-    and make a faster and lighter response
+    we will need them to retrieve only certain data 
+    on some methods to make a faster and lighter response
 
   */
- /* public httpParams:HttpParams=new HttpParams()
-  .set('fields','name,population,flags,capital,cca2');*/
+
+  public httpParams:HttpParams=new HttpParams()
+  .set('fields','name,population,flags,capital,cca2');
   
 
   constructor(private _http:HttpClient) { }
@@ -31,24 +32,27 @@ export class CountryService {
 
     let url:string=`${this.byCountryUrl}/${country}`;
 
-    return this._http.get<Country[]>(url,{/*params:this.httpParams*/});
+    return this._http.get<Country[]>(url,{params:this.httpParams});
   }
 
   searchByCapital(capital:string):Observable<Country[]>{
 
     let url:string=`${this.byCapitalUrl}/${capital}`;
-    return this._http.get<Country[]>(url, {/*params:this.httpParams*/});
+    return this._http.get<Country[]>(url, {params:this.httpParams});
   }
 
   searchByRegion(region:string):Observable<Country[]>{
 
 
     let url:string=`${this.byRegionUrl}/${region}`;
-    return this._http.get<Country[]>(url, {/*params:this.httpParams*/});
+    return this._http.get<Country[]>(url, {params:this.httpParams});
   }
 
-  //in older api by code it returns only an object, but here also an array
-  //here we need the whole object and not certain fields so we use no httpParams
+  /*
+    in older api by code it returns only an object, but here also an array
+    here we need the whole object and not certain fields so we use no httpParams
+
+  */
   getCountryByCode(code:string):Observable<Country[]>{
 
     let url=`${this.countryByCodeUrl}/${code}`;
